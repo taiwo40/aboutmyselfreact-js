98380606aaf370e6b37d665926bc50749b6278e4
taiwo40 <taiwousman5567@gmail.com>
2023-05-10 07:07:14 +0100

    About myself

---
+++ lord/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h3> My name is Taiwo usman olamide i am from oyo state i studied computer enginering in osun state polytechnic iree </h3>
+      <p> i love playing football im a kind hearted perdon and a good man with a great personality </p>
+      <p>i love dressing and i do love watching movies and football for fun my favourites player is <b>lionel messi</b> </p>
+      <img src=" https://cdn.vanguardngr.com/wp-content/uploads/2023/05/Lionel-Messi-PSG.jpg.webp" alt="messi " />
+    </div>
+  )
+       
+}
+
+export default App
